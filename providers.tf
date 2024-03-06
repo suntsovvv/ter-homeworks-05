d471dbb27a3b70e56d0b133241e226809fd1157b
suntsovvv <suntsovvv@gmail.com>
2024-03-06 23:23:07 +1000

    task2 code done

--- providers.tf
+++ providers.tf
@@ -6,6 +6,18 @@ terraform {
   }
   required_version = ">=1.5"
 }
+terraform {
+backend "s3" {
+endpoint = "storage.yandexcloud.net"
+bucket = "svv-bucket"
+region = "ru-central1"
+key = "terraform.tfstate"
+skip_region_validation = true
+skip_credentials_validation = true
+dynamodb_endpoint = "https://docapi.serverless.yandexcloud.net/ru-central1/b1g6dgftb02k9esf1nmu/etn8cb19l9o40jt4k6kd"
+dynamodb_table = "state-table"
+}
+}
 
 provider "yandex" {
   cloud_id  = var.cloud_id
